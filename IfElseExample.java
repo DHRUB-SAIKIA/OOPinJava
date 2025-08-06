e125b9a66bdcb11704b9645333c1fc27595b6453
DHRUBA SAIKIA <dhrubasaikia937@gmail.com>
2025-08-06 10:37:23 +0530

    Add files via upload

---
+++ IfElseExample.java
@@ -0,0 +1,20 @@
+public class IfElseExample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Even or Odd from 1 to 10:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            // Check if the number is even or odd
+            if (i % 2 == 0)
+            {
+                System.out.println(i + " is Even");
+            }
+            else
+            {
+                    System.out.println(i + " is Odd");
+            }
+        }
+    }
+}
