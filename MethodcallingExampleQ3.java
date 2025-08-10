14ebc694a9879c0ba4eb3e0e0d30817817d65002
DHRUBA SAIKIA <dhrubasaikia937@gmail.com>
2025-08-10 18:59:01 +0530

    Add files via upload

---
+++ MethodcallingExampleQ3.java
@@ -0,0 +1,16 @@
+public class MethodcallingExampleQ3
+{
+    static void Display(String name, String address, int age)
+    {
+        System.out.println("My Name is :"+ name);
+        System.out.println("My Address is : " + address);
+        System.out.println("My age is :" + age);
+
+    }
+
+    public static void main(String[] args)
+    {
+        Display("Dhruba saikia","Tezpur",18);
+    }
+
+}
