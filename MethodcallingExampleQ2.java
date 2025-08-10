245815812d5b056a60077fa13820b08de2ba71e1
DHRUBA SAIKIA <dhrubasaikia937@gmail.com>
2025-08-10 18:57:43 +0530

    Add files via upload

---
+++ MethodcallingExampleQ2.java
@@ -0,0 +1,14 @@
+public class MethodcallingExampleQ2
+{
+    static void Display()
+    {
+        System.out.println("Dhruba");
+        System.out.println("Tezpur");
+        System.out.println(18);
+
+    }
+
+    public static void main(String[] args) {
+        Display();
+    }
+}
