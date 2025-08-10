dde944b9f90a5653fe3b8c0bb47b6207c6820b6b
DHRUBA SAIKIA <dhrubasaikia937@gmail.com>
2025-08-10 19:00:32 +0530

    Add files via upload

---
+++ MethodMultipleParametersQ6.java
@@ -0,0 +1,13 @@
+public class MethodMultipleParametersQ6
+{
+    static void myMethod(String fname, int age)
+    {
+        System.out.println(fname + " is " + age);
+    }
+    public static void main(String[ ] args)
+    {
+        myMethod("Dhruba", 19);
+        myMethod("Anurag", 20);
+        myMethod("Anmol", 21);
+    }
+}
