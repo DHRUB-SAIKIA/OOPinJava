c84d19f56993e76f0f0fdae9bd357b89457eb749
DHRUBA SAIKIA <dhrubasaikia937@gmail.com>
2025-08-10 19:00:16 +0530

    Add files via upload

---
+++ MethodParametersQ5.java
@@ -0,0 +1,13 @@
+public class MethodParametersQ5
+{
+    static void myMethod(String fname)
+    {
+        System.out.println(fname +  "Chutya");
+    }
+    public static void main(String[] args)
+    {
+        myMethod("Dhruba");
+        myMethod("Anurag");
+        myMethod("Anmol");
+    }
+}
