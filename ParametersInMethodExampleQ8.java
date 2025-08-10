f9903c7dac126d376064b4f3baf3648d11674d1f
DHRUBA SAIKIA <dhrubasaikia937@gmail.com>
2025-08-10 19:01:25 +0530

    Add files via upload

---
+++ ParametersInMethodExampleQ8.java
@@ -0,0 +1,13 @@
+public class ParametersInMethodExampleQ8
+{
+    static void myMethod(String fname)
+    {
+        System.out.println(fname +  "Chutiya");
+    }
+    public static void main(String[] args)
+    {
+        myMethod("Dhruba ");
+        myMethod("Anurag ");
+        myMethod("Anmol ");
+    }
+}
